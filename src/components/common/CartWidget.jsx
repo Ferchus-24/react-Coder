f0a098b5dee9d3ba945484a292f2865eaf4ee4fe
Ferchus <fernanda.prestinoni@fce.uncu.edu.ar>
2024-07-03 18:26:00 -0300

    cambios19

--- src/components/common/CartWidget.jsx
+++ src/components/common/CartWidget.jsx
@@ -1,18 +1,3 @@
-// import React, { useContext } from "react";
-// import { CartCheck } from "react-bootstrap-icons";
-// import { Link } from "react-router-dom";
-// import { CartContext } from "../../../context/CartContext";
-
-// export const CartWidget = () => {
-//   const { calcularCantidad } = useContext(CartContext);
-
-//   return (
-//     <Link className="carrito" to="/carrito">
-//       <CartCheck /> {calcularCantidad()}
-//     </Link>
-//   );
-// };
-
 import React, { useContext } from "react";
 import { CartCheck } from "react-bootstrap-icons";
 import { Link } from "react-router-dom";
