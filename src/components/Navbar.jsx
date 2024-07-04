bd06a58c3ad1c55bb04139d1a7ceb0905967b8b4
Ferchus <fernanda.prestinoni@fce.uncu.edu.ar>
2024-07-04 17:24:07 -0300

    cambios22

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -30,9 +30,9 @@ export const Navbar = () => {
           type="button"
           data-bs-toggle="collapse"
           data-bs-target="#navbarNav"
-          aria-controls="navbarNav"
-          aria-expanded="false"
-          aria-label="Toggle navigation"
+          // aria-controls="navbarNav"
+          // aria-expanded="false"
+          // aria-label="Toggle navigation"
         >
           <span className="navbar-toggler-icon"></span>
         </button>
